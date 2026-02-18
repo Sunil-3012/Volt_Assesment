b9064d3ce658024b0382e474df5fba83ca302fa4
Gangupamu Sai Sunil <gangupamusaisunil@MacBookAir.attlocal.net>
2026-02-18 09:14:06 -0500

    added my recomendations and cost analysis

--- submission/terraform/cost_optimization.tf
+++ submission/terraform/cost_optimization.tf
@@ -11,8 +11,8 @@ Current Monthly Cost: $47,832
 # Top Saving Oppurtunities:
 ## S3 Storage ($10,350): The vlt-video-chunks-prod bucket has 45TB in Standard tier, but 95% of access happens in the first 30 days. Moving data to Infrequent Access (IA) or Glacier after 30 days will save thousands.
 
-## EC2 Under-utilization ($17,064): Video-processing nodes are at 34% utilization. We should switch these to Spot Instances since Kafka is resilient.
+## EC2 Under-utilization ($17,064): Video processing nodes are at 34% utilization. We should switch these to Spot Instances since Kafka is resilient.
 
-## General nodes are at 22% utilization. These are "right-sizing" candidates (moving from m5.2xlarge to m5.large).
+## General nodes are at 22% utilization. These are right sizing candidates (moving from m5.2xlarge to m5.large).
 
 ## RDS Over-provisioning ($4,856): The Read Replicas are only at 12% utilization. We can downsize these instances.##
